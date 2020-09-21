942571d31642f6cc7c1cd2e646a479631d7bc687
1611Aryan <agupta32_be19@thapar.edu>
2020-09-21 13:04:35 +0530

    :-)

---
+++ dist/magic.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var body = document.querySelector('body');
+var eraser;
+var vw = window.innerWidth;
+$('body').on('mousedown', function (event) {
+  eraser = document.createElement('span');
+  $('body').on('mousemove', function (event) {
+    eraser = document.createElement('span');
+    eraser.style.left = event.clientX - vw * 4 / 100 + 'px';
+    eraser.style.top = event.clientY - vw * 4 / 100 + 'px';
+    body.appendChild(eraser);
+    setTimeout(function () {
+      document.querySelector('span').remove();
+    }, 1000);
+  });
+  $('body').on('mouseup', function (event) {
+    $('span').remove();
+  });
+});
---
+++ magic.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body');
+let eraser;
+const vw = window.innerWidth;
+$('body').on('mousedown', function (event) {
+    eraser = document.createElement('span');
+
+    $('body').on('mousemove', function (event) {
+        eraser = document.createElement('span');
+        eraser.style.left = event.clientX - vw * 4 / 100 + 'px';
+        eraser.style.top = event.clientY - vw * 4 / 100 + 'px';
+        body.appendChild(eraser);
+        setTimeout(function () {
+            document.querySelector('span').remove();
+        }, 1000);
+
+    });
+    $('body').on('mouseup', function (event) {
+
+        $('span').remove();
+
+    });
+});
+
